883e3ddd000fdd4c42d87fc786e61bcf7ee72067
Feliks Kiszkurno <felikskrno@outlook.com>
2021-06-07 16:51:39 +0200

    changes related to debugging

--- slopestabilityML/classification_predict.py
+++ slopestabilityML/classification_predict.py
@@ -111,7 +111,7 @@ def classification_predict(test_prediction, test_results, clf_name, num_feat, *,
             diff = abs(np.ones([len(depth_interface_true)]) * interfaces_detected[interfaces_key][
                 'depth_mean'] - depth_interface_true)
             error_file = open(os.path.join(settings.settings['results_folder'], 'error_file.txt'), 'a')
-            error_file.write(test_name_pred.array_str())
+            error_file.write(test_name_pred)
             error_file.write('\n')
             print(depth_interface_true.array_str())
             error_file.write(depth_interface_true)
